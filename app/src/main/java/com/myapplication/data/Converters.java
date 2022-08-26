97664f88c35c0ebf196dee048104f0d7e1fe0ed3
IdarH <idar-95@hotmail.com>
2022-08-26 16:40:03 +0200

    Slight converter change

--- app/src/main/java/com/myapplication/data/Converters.java
+++ app/src/main/java/com/myapplication/data/Converters.java
@@ -3,15 +3,14 @@ package com.myapplication.data;
 import androidx.room.TypeConverter;
 import java.util.Date;
 
-public class DateConverter {
-
+public class Converters {
     @TypeConverter
-    public static Date toDate(Long timestamp) {
-        return timestamp == null ? null : new Date(timestamp);
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
     }
 
     @TypeConverter
-    public static Long toTimestamp(Date date) {
+    public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
     }
 }
