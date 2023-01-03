1f388bf4bb08cc13d5e190c4c342e668ccadb7a0
IdarH <idar-95@hotmail.com>
2023-01-03 10:40:35 +0100

    Create FirestoreService
    A class where all the uploading of data is done

---
+++ app/src/main/java/com/myapplication/FirestoreService.java
@@ -0,0 +1,13 @@
+package com.myapplication;
+
+import com.myapplication.data.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirestoreService {
+    private  List<User> userList = new ArrayList<User>();
+    private List<Workout> workoutList = new ArrayList<Workout>();
+    private List<Exercise> exerciseList = new ArrayList<Exercise>();
+
+}
